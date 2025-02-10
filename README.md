6ec2d2d4c103b2e72256fd26390752216ab7774d
rjshkhr <rjshekhar.official@gmail.com>
2025-02-10 08:10:47 +0530

    init

---
+++ README.md
@@ -0,0 +1,45 @@
+# Shadow
+
+A dark, muted colorscheme for [Neovim](https://github.com/neovim/neovim).
+
+## Screenshots
+
+![Screenshot](https://imgur.com/UihTWwk.png)
+
+## Installation
+
+Using [lazy.nvim](https://github.com/folke/lazy.nvim):
+
+```lua
+{
+    "rjshkhr/shadow.nvim",
+    priority = 1000,
+    config = function()
+        vim.opt.termguicolors = true
+        vim.cmd.colorscheme("shadow")
+    end,
+}
+```
+
+Using [packer.nvim](https://github.com/wbthomason/packer.nvim):
+
+```lua
+use {
+   "rjshkhr/shadow.nvim",
+   config = function()
+       vim.opt.termguicolors = true
+       vim.cmd.colorscheme("shadow")
+   end
+}
+```
+
+## Supported Plugins
+
+- [telescope.nvim](https://github.com/nvim-telescope/telescope.nvim)
+- [nvim-cmp](https://github.com/hrsh7th/nvim-cmp)
+- [oil.nvim](https://github.com/stevearc/oil.nvim)
+- [dressing.nvim](https://github.com/stevearc/dressing.nvim)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ lua/shadow/init.lua
@@ -0,0 +1,293 @@
+local M = {}
+
+local colors = {
+	bg = { gui = "#1D2326", cterm = 0 },
+	fg = { gui = "#E6E7E6", cterm = 7 },
+	black = { gui = "#242B2D", cterm = 0 },
+	black_bright = { gui = "#485457", cterm = 8 },
+	red = { gui = "#BC8F7D", cterm = 1 },
+	red_bright = { gui = "#D4A394", cterm = 9 },
+	green = { gui = "#96B088", cterm = 2 },
+	green_bright = { gui = "#ABC49E", cterm = 10 },
+	yellow = { gui = "#CCAC7D", cterm = 3 },
+	yellow_bright = { gui = "#E2BF8F", cterm = 11 },
+	blue = { gui = "#7E9AAB", cterm = 4 },
+	blue_bright = { gui = "#94B1C4", cterm = 12 },
+	magenta = { gui = "#A68CAA", cterm = 5 },
+	magenta_bright = { gui = "#BC9EC0", cterm = 13 },
+	cyan = { gui = "#839C98", cterm = 6 },
+	cyan_bright = { gui = "#97B3AF", cterm = 14 },
+	white = { gui = "#CED3DC", cterm = 7 },
+	white_bright = { gui = "#E8EBF0", cterm = 15 },
+}
+
+local highlights = {
+	-- Basic Editor UI
+	Normal = { fg = colors.fg.gui, bg = colors.bg.gui, ctermfg = colors.fg.cterm, ctermbg = colors.bg.cterm }, -- Default text and background
+	SignColumn = { bg = colors.bg.gui, ctermbg = colors.bg.cterm }, -- Column where signs (like git changes) are displayed
+	MsgArea = { fg = colors.fg.gui, bg = colors.bg.gui, ctermfg = colors.fg.cterm, ctermbg = colors.bg.cterm }, -- Area for messages and cmdline
+	ModeMsg = { fg = colors.fg.gui, bg = colors.bg.gui, ctermfg = colors.fg.cterm, ctermbg = colors.bg.cterm }, -- Mode messages (e.g., "-- INSERT --")
+	MsgSeparator = { fg = colors.fg.gui, bg = colors.bg.gui, ctermfg = colors.fg.cterm, ctermbg = colors.bg.cterm }, -- Separator for scrolled messages
+	MatchParen = {
+		fg = colors.cyan.gui,
+		bg = colors.black_bright.gui,
+		ctermfg = colors.cyan.cterm,
+		ctermbg = colors.black_bright.cterm,
+	},
+
+	-- Window separators
+	WinSeparator = { fg = colors.black.gui, ctermfg = colors.black.cterm },
+
+	-- Line Numbers and Cursor
+	LineNr = { fg = colors.black_bright.gui, ctermfg = colors.black_bright.cterm }, -- Line numbers
+	CursorLineNr = { fg = colors.green.gui, ctermfg = colors.green.cterm }, -- Current line number
+	CursorLine = { bg = colors.bg.gui, ctermbg = colors.bg.cterm }, -- Line the cursor is on
+	CursorColumn = { bg = colors.bg.gui, ctermbg = colors.bg.cterm }, -- Column the cursor is on
+
+	-- Search and Selection
+	Search = {
+		fg = colors.bg.gui,
+		bg = colors.blue_bright.gui,
+		ctermfg = colors.bg.cterm,
+		ctermbg = colors.blue_bright.cterm,
+	},
+	IncSearch = {
+		fg = colors.bg.gui,
+		bg = colors.yellow.gui,
+		ctermfg = colors.bg.cterm,
+		ctermbg = colors.yellow.cterm,
+	},
+	Visual = {
+		fg = "NONE",
+		bg = colors.black.gui,
+		ctermfg = "NONE",
+		ctermbg = colors.black.cterm,
+	},
+	VisualNOS = {
+		fg = "NONE",
+		bg = colors.black.gui,
+		ctermfg = "NONE",
+		ctermbg = colors.black.cterm,
+	},
+
+	-- Statusline
+	StatusLine = { fg = colors.fg.gui, bg = colors.bg.gui, ctermfg = colors.fg.cterm, ctermbg = colors.bg.cterm }, -- Active statusline
+	StatusLineNC = {
+		fg = colors.black_bright.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.black_bright.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Inactive statusline
+
+	-- Syntax Highlighting
+	Comment = { fg = colors.black_bright.gui, ctermfg = colors.black_bright.cterm }, -- Comments
+	Constant = { fg = colors.cyan.gui, ctermfg = colors.cyan.cterm }, -- Any constant
+	String = { fg = colors.green.gui, ctermfg = colors.green.cterm }, -- String literals
+	Identifier = { fg = colors.magenta.gui, ctermfg = colors.magenta.cterm }, -- Variable names
+	Function = { fg = colors.blue.gui, ctermfg = colors.blue.cterm }, -- Function names
+	Statement = { fg = colors.magenta.gui, ctermfg = colors.magenta.cterm }, -- Any statement
+	Operator = { fg = colors.fg.gui, ctermfg = colors.fg.cterm }, -- Operators
+	Keyword = { fg = colors.magenta.gui, ctermfg = colors.magenta.cterm }, -- Keywords
+	PreProc = { fg = colors.yellow.gui, ctermfg = colors.yellow.cterm }, -- Preprocessor directives
+	Type = { fg = colors.cyan.gui, ctermfg = colors.cyan.cterm }, -- Type declarations
+	Special = { fg = colors.yellow.gui, ctermfg = colors.yellow.cterm }, -- Special symbols
+
+	-- Popup Menu
+	Pmenu = { fg = colors.fg.gui, bg = colors.black.gui, ctermfg = colors.fg.cterm, ctermbg = colors.black.cterm }, -- Popup menu normal item
+	PmenuSel = {
+		fg = colors.bg.gui,
+		bg = colors.cyan.gui,
+		ctermfg = colors.bg.cterm,
+		ctermbg = colors.cyan.cterm,
+	}, -- Popup menu selected item
+	PmenuSbar = { bg = colors.black.gui, ctermbg = colors.black.cterm }, -- Popup menu scrollbar
+	PmenuThumb = { bg = colors.cyan.gui, ctermbg = colors.cyan.cterm }, -- Popup menu scrollbar thumb
+
+	-- Floating windows
+	FloatTitle = {
+		fg = colors.blue.gui,
+		bg = colors.black.gui,
+		ctermfg = colors.blue.cterm,
+		ctermbg = colors.black.cterm,
+	}, -- Floating window title
+	FloatBorder = {
+		fg = colors.black.gui,
+		bg = colors.black.gui,
+		ctermfg = colors.black.cterm,
+		ctermbg = colors.black.cterm,
+	}, -- Floating window border
+	NormalFloat = { fg = colors.fg.gui, bg = colors.black.gui, ctermfg = colors.fg.cterm, ctermbg = colors.black.cterm }, -- Normal text in floating windows
+
+	-- Diagnostics
+	DiagnosticError = { fg = colors.red.gui, ctermfg = colors.red.cterm }, -- Error diagnostics
+	DiagnosticWarn = { fg = colors.yellow.gui, ctermfg = colors.yellow.cterm }, -- Warning diagnostics
+	DiagnosticInfo = { fg = colors.blue.gui, ctermfg = colors.blue.cterm }, -- Information diagnostics
+	DiagnosticHint = { fg = colors.cyan.gui, ctermfg = colors.cyan.cterm }, -- Hint diagnostics
+
+	DiagnosticUnderlineError = { sp = colors.red.gui, undercurl = true }, -- Error underline
+	DiagnosticUnderlineWarn = { sp = colors.yellow.gui, undercurl = true }, -- Warning underline
+	DiagnosticUnderlineInfo = { sp = colors.blue.gui, undercurl = true }, -- Info underline
+	DiagnosticUnderlineHint = { sp = colors.cyan.gui, undercurl = true }, -- Hint underline
+
+	-- Status line
+	StatusLineMode = { fg = colors.blue.gui, ctermfg = colors.blue.cterm }, -- Mode indicator
+	StatusLinePath = { fg = colors.black_bright.gui, ctermfg = colors.black_bright.cterm }, -- File path
+	StatusLineFlags = { fg = colors.yellow.gui, ctermfg = colors.yellow.cterm }, -- File flags
+	StatusLineFileType = { fg = colors.black_bright.gui, ctermfg = colors.black_bright.cterm }, -- File type
+	StatusLinePosition = { fg = colors.magenta.gui, ctermfg = colors.magenta.cterm }, -- Line and column
+	StatusLinePercent = { fg = colors.green.gui, ctermfg = colors.green.cterm }, -- Percentage
+	StatusLineModified = { fg = colors.yellow.gui, ctermfg = colors.yellow.cterm }, -- Color for modified icon
+
+	StatusLineModeNormal = { fg = colors.blue.gui, ctermfg = colors.blue.cterm },
+	StatusLineModeInsert = { fg = colors.green.gui, ctermfg = colors.green.cterm },
+	StatusLineModeVisual = { fg = colors.yellow.gui, ctermfg = colors.yellow.cterm },
+	StatusLineModeCommand = { fg = colors.green.gui, ctermfg = colors.green.cterm },
+	StatusLineModeReplace = { fg = colors.red.gui, ctermfg = colors.red.cterm },
+	StatusLineModeTerminal = { fg = colors.cyan.gui, ctermfg = colors.cyan.cterm },
+
+	-- nvim-telescope
+	TelescopeNormal = {
+		fg = colors.fg.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.fg.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Telescope window text
+	TelescopePromptNormal = {
+		fg = colors.fg.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.fg.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Prompt window text
+	TelescopeResultsNormal = {
+		fg = colors.fg.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.fg.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Results window text
+	TelescopeSelection = {
+		fg = colors.fg.gui,
+		bg = colors.black.gui,
+		ctermfg = colors.fg.cterm,
+		ctermbg = colors.black.cterm,
+	}, -- Selected item
+	TelescopeMatching = { fg = colors.yellow.gui, ctermfg = colors.yellow.cterm }, -- Matched characters in search
+
+	-- nvim-cmp
+	CmpItemAbbr = { fg = colors.fg.gui, ctermfg = colors.fg.cterm }, -- Completion menu text
+	CmpItemAbbrDeprecated = { fg = colors.black_bright.gui, ctermfg = colors.black_bright.cterm, strikethrough = true }, -- Deprecated items
+	CmpItemMenu = { fg = colors.black_bright.gui, ctermfg = colors.black_bright.cterm }, -- Menu text (e.g., [LSP])
+
+	CmpItemKindVariable = {
+		fg = colors.magenta.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.magenta.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Variables
+	CmpItemKindFunction = {
+		fg = colors.blue.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.blue.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Functions
+	CmpItemKindMethod = {
+		fg = colors.blue.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.blue.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Methods
+	CmpItemKindClass = {
+		fg = colors.yellow.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.yellow.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Classes
+	CmpItemKindInterface = {
+		fg = colors.yellow.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.yellow.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Interfaces
+	CmpItemKindModule = {
+		fg = colors.yellow.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.yellow.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Modules
+	CmpItemKindProperty = {
+		fg = colors.magenta.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.magenta.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Properties
+	CmpItemKindField = {
+		fg = colors.magenta.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.magenta.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Fields
+	CmpItemKindEnum = {
+		fg = colors.yellow.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.yellow.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Enums
+	CmpItemKindSnippet = {
+		fg = colors.cyan.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.cyan.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Snippets
+	CmpItemKindFile = { fg = colors.fg.gui, bg = colors.bg.gui, ctermfg = colors.fg.cterm, ctermbg = colors.bg.cterm }, -- Files
+	CmpItemKindFolder = { fg = colors.fg.gui, bg = colors.bg.gui, ctermfg = colors.fg.cterm, ctermbg = colors.bg.cterm }, -- Folders
+	CmpItemKindKeyword = {
+		fg = colors.magenta.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.magenta.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Keywords
+	CmpItemKindConstant = {
+		fg = colors.cyan.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.cyan.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Constants
+	CmpItemKindOperator = {
+		fg = colors.cyan.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.cyan.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Operators
+	CmpItemKindReference = {
+		fg = colors.magenta.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.magenta.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- References
+	CmpItemKindValue = {
+		fg = colors.cyan.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.cyan.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Values
+	CmpItemKindUnit = {
+		fg = colors.cyan.gui,
+		bg = colors.bg.gui,
+		ctermfg = colors.cyan.cterm,
+		ctermbg = colors.bg.cterm,
+	}, -- Units
+
+	-- oil.nvim
+	OilDir = { fg = colors.blue.gui, ctermfg = colors.blue.cterm }, -- Directory names
+	OilFile = { fg = colors.fg.gui, ctermfg = colors.fg.cterm }, -- File names
+	OilLink = { fg = colors.cyan.gui, ctermfg = colors.cyan.cterm }, -- Symbolic links
+}
+
+M.colors = colors
+
+function M.setup()
+	for group, settings in pairs(highlights) do
+		vim.api.nvim_set_hl(0, group, settings)
+	end
+end
+
+return M
